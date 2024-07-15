ee348d7e6950bbffa70807daca15c5053c766b4c
Saumya13-5 <120590759+Saumya13-5@users.noreply.github.com>
2024-07-15 20:25:57 +0530

    Add files via upload
    These files just contain the basic structure for login and registration page. The css needs to be changed significantly and also need to make changes in the security of the forms.....

---
+++ symphony/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.welcome_view, name='welcome'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+]
